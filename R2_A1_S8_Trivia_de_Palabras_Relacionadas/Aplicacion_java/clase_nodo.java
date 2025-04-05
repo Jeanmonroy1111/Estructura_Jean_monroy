e995bfe6fa8590f273d1688cf5e1fd68b61c55a4
Jeanmonroy1111 <jschneidermonroy@ucundinamarca.edu.co>
2025-04-04 22:41:24 -0500

    cracion clase nodo
    creación de esta clase para realizar la conexión de la lista por medio de la estructura enlazada en lugar de una común array

---
+++ R2_A1_S8_Trivia_de_Palabras_Relacionadas/Aplicacion_java/clase_nodo.java
@@ -0,0 +1,11 @@
+package R2_A1_S8_Trivia_de_Palabras_Relacionadas.Aplicacion_java;
+
+public class clase_nodo {
+    clase_cliente cliente;
+    clase_nodo siguiente;
+
+public clase_nodo(clase_cliente cliente){
+    this.cliente = cliente;
+    this.siguiente = null; 
+}
+}
