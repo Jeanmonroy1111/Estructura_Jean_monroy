803d0eca6956280891c828cfdc75308771c80cb1
Jeanmonroy1111 <jschneidermonroy@ucundinamarca.edu.co>
2025-04-13 00:34:37 -0500

    actualizacion ver lista
    mejora en como se ve la opción de ver lista agregando la lógica para ver en que dirección de se esta viendo si de derecha  a izquierda o viceversa

--- R2_A2_S9_Diagrama_de_Flujo/lista_doble.java
+++ R2_A2_S9_Diagrama_de_Flujo/lista_doble.java
@@ -3,6 +3,7 @@ package R2_A2_S9_Diagrama_de_Flujo;
 public class lista_doble {
  private cliente inicio;
  private cliente fin;
+ private String ultimorecorrido="ninguno";
 public lista_doble(){
     inicio=null;
     fin=null;
@@ -42,6 +43,7 @@ public void listaderecha(){
         System.out.println("Cedula: "+actual.cedula+",Nombre: "+actual.nombre);
         actual=actual.siguiente;
     }
+    ultimorecorrido="derecha";
 }
 public void listaizquierda(){
     if (inicio==null) {
@@ -53,6 +55,41 @@ public void listaizquierda(){
     while (actual!=null) {
         System.out.println("Cedula: "+actual.cedula+",Nombre: "+actual.nombre);
         actual=actual.anterior;
+    }
+    ultimorecorrido="izquierda";
+}
+public void verlista(){
+    if(inicio==null){
+        System.out.println("La lista esta vacia");
+        return;
+    }
+    int contador=0;
+    cliente actual= inicio;
+    while (actual!=null) {
+        contador++;
+        actual=actual.siguiente;
+    }
+    System.out.println("Resumen de la lista:");
+    System.out.println("-Total clientes: "+contador);
+    System.out.println("-Primer clienter: "+inicio.nombre+"(Cedula: "+inicio.cedula+")");
+    System.out.println("-ultimos clienter: "+fin.nombre+"(Cedula: "+fin.cedula+")");
+    if (ultimorecorrido.equals("derecha")) {
+        System.out.println("Mostrando lista de izquierda a derecha:");
+        actual=inicio;
+        while (actual!= null) {
+            System.out.println("Cedula: "+actual.cedula+",Nombre"+actual.nombre);
+            actual=actual.siguiente;
+        }
+    }
+    else if (ultimorecorrido.equals("izquierda")) {
+        System.out.println("Mostrando lista derecha a izquierda:");
+        actual=fin;
+        while (actual!= null) {
+            System.out.println("Cedula: "+actual.cedula+",Nombre"+actual.nombre);
+            actual=actual.anterior;
+        }  
+    }else{
+        System.out.println("Aun no se ha realizado un recorrido en el listado");
     }
 }
 }
